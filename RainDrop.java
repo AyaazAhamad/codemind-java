8bf9e05ed3df00b96b8796a458a5708fa0add50e
AyaazAhamad <100121514+AyaazAhamad@users.noreply.github.com>
2022-09-30 15:58:52 +0530

    import java.util.Scanner; class Season {     public static void main(String args[])     {         int a;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         if(a%3==0&&a%5==0&&a%7==0)         {             System.out.println("PlingPlangPlong");         }         else if(a%3==0&&a%5==0)         {             System.out.println("PlingPlang");         }         else if(a%3==0&&a%7==0)         {             System.out.println("PlingPlong");         }         else if(a%5==0&&a%7==0)         {             System.out.println("plangplong");         }         else if (a%3==0)         {             System.out.println("Pling");         }         else if(a%5==0)         {             System.out.println("Plang");         }         else if(a%7==0)         {             System.out.println("Plong");         }         else         {             System.out.println(a);         }         sc.close();     } }

---
+++ RainDrop.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+class Season
+{
+    public static void main(String args[])
+    {
+        int a;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        if(a%3==0&&a%5==0&&a%7==0)
+        {
+            System.out.println("PlingPlangPlong");
+        }
+        else if(a%3==0&&a%5==0)
+        {
+            System.out.println("PlingPlang");
+        }
+        else if(a%3==0&&a%7==0)
+        {
+            System.out.println("PlingPlong");
+        }
+        else if(a%5==0&&a%7==0)
+        {
+            System.out.println("plangplong");
+        }
+        else if (a%3==0)
+        {
+            System.out.println("Pling");
+        }
+        else if(a%5==0)
+        {
+            System.out.println("Plang");
+        }
+        else if(a%7==0)
+        {
+            System.out.println("Plong");
+        }
+        else
+        {
+            System.out.println(a);
+        }
+        sc.close();
+    }
+}
