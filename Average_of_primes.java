68b4bd50252aea9f324a36fd541f1e73e346a30d
AyaazAhamad <100121514+AyaazAhamad@users.noreply.github.com>
2022-10-15 13:47:29 +0530

    import java.util.Scanner; class sample {     public static boolean prime(int n)     {         if(n<2)             return false;         for(int i=2;i<=Math.sqrt(n);i++)         {             if(n%i==0)                 return false;         }         return true;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in); 		int n=sc.nextInt();         int a[]=new int[n];         int sum=0,c=0;         for(int i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         for(int i=0;i<n;i++)         {             if(prime(a[i]))             {                 sum+=a[i];                 c++;             }         }         float avg=(float)sum/c;         System.out.format("%.2f",avg);     } }

---
+++ Average_of_primes.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+class sample
+{
+    public static boolean prime(int n)
+    {
+        if(n<2)
+            return false;
+        for(int i=2;i<=Math.sqrt(n);i++)
+        {
+            if(n%i==0)
+                return false;
+        }
+        return true;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+        int a[]=new int[n];
+        int sum=0,c=0;
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(prime(a[i]))
+            {
+                sum+=a[i];
+                c++;
+            }
+        }
+        float avg=(float)sum/c;
+        System.out.format("%.2f",avg);
+    }
+}
